b6b9e4271b65d62baf1518dc2ff9b47a57578737
OmarRaihan <omarr712007@gmail.com>
2026-05-21 18:43:08 +0300

    Add files via upload

---
+++ DuplicatesHandling/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DuolicatesHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>();
+            Console.Write("How many numbers? ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            while (numbers.Count < size)
+            {
+                try
+                {
+                    Console.Write("Enter number: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    if (numbers.Contains(num))
+                        throw new Exception("Can't accept duplicate numbers");
+
+                    numbers.Add(num);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ VowelsHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace VowelsHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a word: ");
+                string word = Console.ReadLine().ToLower();
+                StringHandle(word);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+        static void StringHandle(string s) 
+        {
+            if (!(s.Contains('a') || s.Contains('e') || s.Contains('i') || s.Contains('o') || s.Contains('u')))
+                throw new Exception("The word should contain vowels");
+        }
+    }
+}
