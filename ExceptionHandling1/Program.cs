84d7f39eeb4b4d85ae7325de00d0a5fb74e33d9b
OmarRaihan <omarr712007@gmail.com>
2026-05-21 18:26:50 +0300

    Add files via upload

---
+++ ExceptionHandling1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ExceptionHandling1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>();
+            Console.Write("How many numbers? ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            while (numbers.Count < size)
+            {
+                try
+                {
+                    Console.Write("Enter number: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    if (numbers.Contains(num))
+                        throw new Exception("Can't accept duplicate numbers");
+
+                    numbers.Add(num);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+            
+            //List<int> nums = new List<int>(6);
+            //int num = 0;
+            //try
+            //{
+            //    while (!nums.Contains(num))
+            //    {
+            //        Console.Write("enter: ");
+            //        num = Convert.ToInt32(Console.ReadLine());
+            //        nums.Add(num);
+            //    }
+            //}
+            //catch(DuplicateWaitObjectException)
+            //{
+            //    Console.WriteLine("can't enter duplicates, try again");
+            //}
+
+
+
+
+
+        }
+    }
+}
